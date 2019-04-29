6d48f1818208a282f90139ae02c189ef217f41f1
clabroche <corentinlabroche@gmail.com>
2019-04-29 12:08:15 +0200

    clean workspace

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ if (process.argv.length < 3) {
 const pathfs = require('path')
 const fse = require('fs-extra')
 const asar = require('asar')
+const Promise = require('bluebird')
+
 ;(async _ => {
   const srcPath = pathfs.resolve(process.argv.pop())
   const srcAsarPathFile = pathfs.resolve(srcPath, 'resources', 'app.asar')
@@ -14,18 +16,27 @@ const asar = require('asar')
   const destAsarUnpackFile = pathfs.resolve(__dirname, 'app.asar.unpacked')
   const destAsarFilePack = pathfs.resolve(__dirname, 'app.asar')
   const pathExist = await fse.exists(srcAsarPathFile)
+
   if (!pathExist) { throw new Error('Path seems to be wrong') }
-  await fse.remove(destAsarPathUnpackDirectory)
-  await fse.remove(destAsarUnpackFile)
-  await fse.remove(destAsarFilePack)
+
+  await cleanWorkspace(destAsarPathUnpackDirectory, destAsarUnpackFile, destAsarFilePack)
   await fse.mkdirp(destAsarPathUnpackDirectory)
   await fse.copy(srcAsarPathFile, destAsarFilePack)
   await fse.copy(srcAsarUnpackFile, destAsarUnpackFile)
   await asar.extractAll(destAsarFilePack, destAsarPathUnpackDirectory)
+
   const utilsPath = pathfs.resolve(destAsarPathUnpackDirectory, 'frontend' ,'utils', 'index.js')
   let utils = await fse.readFile(utilsPath, 'utf-8')
   utils = utils.replace(/noLicAndTrialExpires.*\}\}/, 'noLicAndTrialExpires: false}}')
   await fse.writeFile(utilsPath, utils, 'utf-8')
+
   await asar.createPackage(destAsarPathUnpackDirectory, destAsarFilePack)
   await fse.copy(destAsarFilePack, srcAsarPathFile)
+  await cleanWorkspace(destAsarPathUnpackDirectory, destAsarUnpackFile, destAsarFilePack)
 })().catch(console.error)
+
+async function cleanWorkspace(...paths) {
+  return Promise.map(paths, async path => {
+    await fse.remove(path)
+  })
+}
